9d075349681bdd3bc8075858d8813e42f0c7b832
GAbreu <gabrielabreu571@gmail.com>
2023-07-10 15:50:28 -0300

    Primeiro commit

---
+++ ProjetoICC.c
@@ -0,0 +1,36 @@
+/*
+Trabalho feito para a disciplina: Introdução a Ciência da Computação 1
+Professor: Rudinei Goulart
+Integrantes: Gabriel de Andrade Abreu NUMUSP: 14571362
+             Guilherme Antonio Costa Bandeira NUMUSP: 14575620 
+             Antônio Carlos de Almeida Micheli Neto NUMUSP: 14559013
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct prod{
+    char nomeProd[100]; 
+    unsigned long long int quantidade;
+    double preco;
+};
+
+typedef struct prod produtos;
+
+// adicionar funcões
+
+// usaremos arquivos
+
+int main(void){
+
+    // etapas:
+    // 1
+    // 2
+    // 3
+    // 4
+    // 5
+    // 6
+    // 7
+
+    return 0;
+}
