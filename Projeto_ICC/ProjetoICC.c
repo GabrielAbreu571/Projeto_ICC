77b01366b6a01c8693dde39447c487f838d740af
gabriel_abreu <63174449+GabrielAbreu571@users.noreply.github.com>
2023-07-10 15:46:05 -0300

    Add files via upload

---
+++ Projeto_ICC/ProjetoICC.c
@@ -0,0 +1,24 @@
+/*
+Trabalho feito para a disciplina: Introdução a Ciência da Computação 1
+Professor: Rudinei Goulart
+Integrantes: Gabriel de Andrade Abreu NUMUSP: 14571362
+             Guilherme NUMUSP: 
+             Antônio  NUMUSP:
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+
+    // etapas:
+    // 1
+    // 2
+    // 3
+    // 4
+    // 5
+    // 6
+    // 7
+
+    return 0;
+}
