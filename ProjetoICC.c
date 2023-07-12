b377d64234b14a805d0021d2d6af7a582e5cf300
GAbreu <gabrielabreu571@gmail.com>
2023-07-11 22:43:43 -0300

    #6 adicionando arquivos e corrigindo o código

--- ProjetoICC.c
+++ ProjetoICC.c
@@ -18,18 +18,18 @@ struct prod{
 typedef struct prod produto;
 
 // Protótipos das funções
-
+/*
 produto *alocaProduto(unsigned int estoque);
-void inserirProduto(char *nome, unsigned int quantidade, double preco);
+void inserirProduto(produto** dados);//char *nome, unsigned int quantidade, double preco
 void aumentaEstoque(produto *a, unsigned long long int quantidade);
 void modificaPreco(produto *a, double valor);
 void realizaVenda();
 void consuntaEstoque();
 void consultaSaldo();
 void finalizaDia();
-
-// usaremos arquivos
-
+*/
+// usaremos arquivos (binários)
+/*
 produto *alocaProduto(unsigned int estoque){
     produto *prod;
 
@@ -37,27 +37,30 @@ produto *alocaProduto(unsigned int estoque){
         printf("Erro ao alocar memória");
         exit (1);
     }
-
+//precisa tambem realocar dinâmicamente o vetor de produtos
     return prod; 
 }
-
-void inserirProduto(char *nome, unsigned int quantidade, double preco){
-    scanf("%s %llu %lf", a.nomeProd, &(a.quantidade), &(a.preco));    
+*/
+void inserirProduto(produto** dados,unsigned int qtd/char *nome, unsigned int quantidade, double preco/){ 
+    //tem que passar o ponteiro do produto
+    int t=qtd;
+    (dados)=(produto)realloc(*dados,(t+1)*sizeof(produto));
+    scanf("%s %llu %lf", (*dados)[t].nomeProd, &((*dados)[t].quantidade), &((*dados)[t].preco));    
 }   
-
+/*
 void aumentaEstoque(produto *a, unsigned long long int quantidade){            
-    a.quantidade = a.quantidade + quantidade;
+    a.quantidade = a.quantidade + quantidade;//derreferenciar
 }
 
 void modificaPreco(produto *a, double valor){
-    a.preco = valor;
+    a.preco = valor;//derreferenciae?
 }
 
 void finalizaDia(){
     // mexer com os arquivos
     exit(1);
 }
-
+*/
 int main(void){
     unsigned int qtdEstoque;
     long long int saldo;
@@ -67,22 +70,42 @@ int main(void){
  
     // etapas:
    
-    if(fp = fopen()){
+    fp = fopen("ArquivoEstoque.txt","r");  
 
-    } else if(!= NULL){
+    if(fp!=NULL){
+        fscanf(fp,"%u",&qtdEstoque);
+        fscanf(fp,"%lld",&saldo);
+
+        for(int i=0;i<qtdEstoque;i++){
+             fscanf(fp," %s",dados[i].nomeProd);
+             fscanf(fp," %llu",&dados[i].quantidade); 
+             fscanf(fp," %lf",&dados[i].preco);
+        }
+       fclose(fp);
+      fp= fopen("ArquivoEstoque.txt","w");
+         if(fp==NULL){
+           printf("erro na abertura do arquivo");
+           exit(1);
+         }
+            
+    } else{
+        fp=fopen("ArquivoEstoque.txt","w");
         scanf("%u %lld", &qtdEstoque, &saldo);
-    } 
+    }
+     
+    inserirProduto(&dados,qtdEstoque);
 
+/*
     while(1){
         scanf("%s", id);
 
         if(id == "IP"){
             dados = alocaProduto(qtdEstoque);
-
+            qtdEstoque++;
             if(qtdEstoque > 0){
                 for(int i = 0; i < qtdEstoque; i++){
                     scanf("%s %u %lf", );
-                    dados[i] = inserirProduto(dados[i].nomeProd, dados[i].)
+                    dados[i] = inserirProduto(dados[i].nomeProd, dados[i].);
                 }
             } else{
                 scanf("%s %u %lf", );
@@ -112,11 +135,15 @@ int main(void){
             finalizaDia();
         }
     }
-    
+    */
     // 3
     // 4
     // 5
     // 6
     // 7
+    fprintf(fp,"%u\n",qtdEstoque);
+    fprintf(fp,"%lld\n",saldo);
+    
+    fclose(fp);
     return 0;
 }
